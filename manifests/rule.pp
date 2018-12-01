9f417a57cc322f5888e7207c40e5c426a16d9f8d
James Stuart <james@stuart.name>
2018-12-01 01:13:57 +0000

    stubs for base class and rule

---
+++ manifests/rule.pp
@@ -0,0 +1,9 @@
+# A description of what this defined type does
+#
+# @summary A short summary of the purpose of this defined type.
+#
+# @example
+#   local_fw::rule { 'namevar': }
+define local_fw::rule(
+) {
+}
